0d524467f5534e5538e25d2ba19f1e562f0edbd0
tech <tech@salt1>
2024-08-13 19:33:59 -0400

    daily commit

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TenantController;
 
 Route::group(['prefix' => 'auth'], function () {
     
@@ -10,35 +11,16 @@ Route::group(['prefix' => 'auth'], function () {
     Route::post('register', [AuthController::class, 'register']);
 
     Route::group(['middleware' => 'auth:sanctum'], function() {
-      Route::get('logout', [AuthController::class, 'logout']);
-      Route::get('user', [AuthController::class, 'user']);
-    });
+        Route::get('logout', [AuthController::class, 'logout']);
+        Route::get('user', [AuthController::class, 'user']);
+    });   
+});    
+
+Route::group(['middleware' => 'auth:sanctum'], function() {
+    Route::get('tenants', [TenantController::class, 'index']);
+    Route::get('tenants/{tenant}', [TenantController::class, 'show']);
+    Route::post('tenants', [TenantController::class, 'save']);
+    Route::put('tenants/{tenant}', [TenantController::class, 'update']);
+    Route::delete('tenants/{tenant}', [TenantController::class, 'delete']);
 });
 
-/*
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-*/
-/*
-use App\Http\Controllers\AuthController;
-   
-Route::controller(AuthController::class)->group(function(){
-    Route::post('register', 'register');
-    Route::post('login', 'login');
-});
-
-
-Route::group([
-    "middleware" => ["auth:sanctum"]
-],function(){
-    Route::post("profile",[AuthController::class,"profile"]);
-    Route::post("logout",[AuthController::class,"logout"]);
-});
-
-/*
-use App\Http\Controllers\ProductController;
-Route::middleware('auth:sanctum')->group( function () {
-    Route::resource('products', ProductController::class);
-});
-*/
